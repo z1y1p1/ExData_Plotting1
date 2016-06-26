30334b1e42b2b564740a139ec42efc178fc7f43a
z1y1p1 <zhaohelen@hku.hk>
2016-06-27 03:16:49 +0800

    Update Plot 4.R

--- Plot 4.R
+++ Plot 4.R
@@ -1,9 +1,13 @@
 # Plot 4
 par(mfrow = c(2, 2))
+#as in plot 1
 plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+#as in plot 2
 plot(data$DateTime, data$Voltage, xlab = 'datetime', ylab = 'Voltage', type = 'l')
+#as in plot 3
 plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
 lines(data$DateTime, data$Sub_metering_2, col = 'red')
 lines(data$DateTime, data$Sub_metering_3, col = 'blue')
 legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1, cex=0.5, pch=1)
+#plot 4
 plot(data$DateTime, data$Global_reactive_power, xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
