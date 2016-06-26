7215a557d35c73b3fa84ea62e08112314082f46f
z1y1p1 <zhaohelen@hku.hk>
2016-06-27 03:07:30 +0800

    Create Plot 2.R

---
+++ Plot 2.R
@@ -0,0 +1,9 @@
+# Plot 2
+# convert data and time to specific format
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+head(data$DateTime)
+Weekday<-format(data$DateTime, "%a %b %c")
+#make the plot
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
