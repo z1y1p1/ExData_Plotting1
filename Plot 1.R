1df74ddbe192848082e7bbae2ee56e655ffdd997
z1y1p1 <zhaohelen@hku.hk>
2016-06-27 03:10:21 +0800

    Update and rename Assignment1_R.Codes to Plot 1.R

---
+++ Plot 1.R
@@ -0,0 +1,17 @@
+EFA Assignment project 1
+plot1.R
+# download data
+getwd()
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip('./data/household_power_consumption.zip', exdir = './data')
+#read data
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+# Plot 1
+
+# plot figure
+# Plot 1
+hist(data$Global_active_power,breaks=20, xlab="Global Active Power(kilowatts)", col="red", main="Global Active Power")
+
